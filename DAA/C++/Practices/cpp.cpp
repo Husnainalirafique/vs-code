ca6381cdf37fbb8bc89cf5d3daeef4a7d85856fd
Husnain Ali Rafique <victoryhouse125@gmail.com>
2022-01-01 21:11:09 +0500

    Updated

--- DAA/C++/Practices/cpp.cpp
+++ DAA/C++/Practices/cpp.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+struct cpp
+{
+    int i;
+    float f;
+};
+
 
 int main()
 {
-    
+    cpp c;
+    c.i = 10;
+    c.f = 20.5;
+    cout << c.i << " " << c.f << endl;
+    delete &c;
+
     return 0;
 }
--- Java/Husnain.java
+++ Java/Husnain.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
-
 public class Husnain {
 
     public static void main(String[] args) {
+        System.out.println("Husnain");
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the name =");
-        String name=in.nextLine();
-        System.out.println(name);
-        
+        int arr[];
+        arr = new int[5];
+        //store the value in array by using for loop
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter the value of array = ");
+            arr[i] = in.nextInt();
+        }
+        //print the value of array by using for loop
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
