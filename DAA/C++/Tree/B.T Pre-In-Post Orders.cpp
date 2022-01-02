ab646efa7c1eeefcb9618da60d933b2ce8f63fcf
Husnain Ali Rafique <victoryhouse125@gmail.com>
2022-01-02 12:59:35 +0500

    a

--- DAA/C++/Tree/B.T Pre-In-Post Orders.cpp
+++ DAA/C++/Tree/B.T Pre-In-Post Orders.cpp
@@ -30,25 +30,40 @@ void inorder(struct node* root){
     cout<<root->data<<" ";
     inorder(root ->right);
 }
-
+void postorder(struct node* root){
+    if(root == NULL)
+    {
+        return;
+    }
+    postorder(root -> left);
+    postorder(root ->right);
+    cout<<root->data<<" ";
+}
 int main()
 {
     /*
+       
            1
          /   \
         2     3
        / \   / \
       4   5 6   7
+    
     */
-    struct node *root = new node(1);
+    struct node* root = new node(1);
     root->left = new node(2);
     root->right = new node(3);
     root->left->left = new node(4);
     root->left->right = new node(5);
     root->right->left = new node(6);
     root->right->right = new node(7);
-
-    // preorder(root);
+    cout<<"pre order"<<endl;
+    preorder(root);
+    cout<<endl;
+    cout<<"in order"<<endl;
     inorder(root);
+    cout<<endl;
+    cout<<"post order"<<endl;
+    postorder(root);
     return 0;
 }
