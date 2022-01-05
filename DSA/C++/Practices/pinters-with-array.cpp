8b1e688e512a5ee83117eb3c24f2f2f5f823ab97
Husnain Ali Rafique <victoryhouse125@gmail.com>
2022-01-05 20:14:02 +0500

    updated

---
+++ DSA/C++/Practices/pinters-with-array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *ptr;
+    ptr = arr;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "arr[" << i << "] = " << ptr + i << endl;
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "arr[" << i << "] = " << *ptr+i <<endl;
+    }
+    return 0;
+}
