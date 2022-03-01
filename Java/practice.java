390f2db2b6d4dddfa0b5715709980aaddc28497e
Husnain Ali Rafique <victoryhouse125@gmail.com>
2022-03-01 07:37:34 +0500

    updated

--- Java/practice.java
+++ Java/practice.java
@@ -1,5 +1,14 @@
+// import java.util.Scanner;
 public class practice {
     public static void main(String[] args) {
-        System.out.println("Husnain");
+        // (For each) loop concept
+        int sum = 0;
+        int nums[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int x : nums) {
+            System.out.println("value = " + x);
+            sum =sum + x;
+        //All the values of nums here moves to x
+        }
+        System.out.println("Sumation = " + sum);
     }
 }
