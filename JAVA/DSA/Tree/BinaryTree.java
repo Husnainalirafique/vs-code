1f4cfbab5aa3df6aebd1afb81f429172b264ee6a
Husnain <husnainalirafique125@gmail.com>
2022-11-15 09:56:20 +0500

    updated

--- JAVA/DSA/Tree/BinaryTree.java
+++ JAVA/DSA/Tree/BinaryTree.java
@@ -1,7 +1,31 @@
 package Tree;
 
 public class BinaryTree {
+    static class node {
+        int data;
+        node left, right;
+
+        node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    public static void inorder(node root) {
+        if (root == null) {
+            return;
+        } else {
+            inorder(root.left);
+            System.out.print(root.data + " - >");
+            inorder(root.right);
+        }
+    }
+
     public static void main(String[] args) {
-        
+        node root = new node(1);
+        root.left = new node(2);
+        root.right = new node(3);
+
+        inorder(root);
     }
 }
